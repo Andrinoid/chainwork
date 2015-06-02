f8f7defd745493c42a283c3fd437f79891d4b155
Andri Birgisson <andri@tweecode.com>
2015-06-02 21:09:25 +0000

    Component run added. typeOf function fixed to handle strings

--- chainwork.js
+++ chainwork.js
@@ -15,11 +15,9 @@ var typeOf = function(item) {
     'use strict';
     if (item === null) {
         return 'null';  
-    } 
-    if (item.$family) {
-        return item.$family();
     }
-
+    if (typeof(item) === 'string')
+        return 'string';
     if (item.nodeName) {
         if (item.nodeType === 1) {
             return 'element';
@@ -44,6 +42,7 @@ var ChainWork = (function () {
         var options = options || {};
         this.debug = options['debug'] || false;
         this.autoPlay = options['autoPlay'] || false;
+        this.onComplete = options['onComplete'] || function(){};
         //Constructor
         this.chain = [];
         this.isBusy = false;
@@ -127,6 +126,7 @@ var ChainWork = (function () {
         //chain has reached the end
         if(this.index >= this.chain.length) {
             if(this.debug) console.log('chain is being called but out of range');
+            this.onComplete(this.collection); 
             return;
         }
         this.caller = caller || 'user'; // if caller is not defined we asume its a user action
@@ -139,8 +139,8 @@ var ChainWork = (function () {
         //if dependancies are listed run them before
         var depsErrorList = this.checkDependancies();
         if(depsErrorList.length) {
-            console.warn(this.chain[this.index].componentName, 'is missing dependancy, please add them before. Missing:'+ depsErrorList.toString());
-            return false;
+            if(this.debug)
+                console.warn(this.chain[this.index].componentName, 'might be missing dependancy, please add them before. Missing:'+ depsErrorList.toString());
         }
         this.applySettings();
         //inject the this class as parent of all components. so components can access it with this.parent
@@ -271,3 +271,26 @@ var ChainWork = (function () {
 
     return ChainWork;
 })();
+
+
+var Component = (function() {
+    
+    function Component() {
+        this.run = function(name, settings) {
+            var component = {
+                componentName: name,
+                settings: settings
+            }
+            var link = new ChainWork();
+            link.add(component);
+            link.play();
+        }
+    }
+    return Component;
+
+})();
+
+var Component = new Component();
+
+
+
